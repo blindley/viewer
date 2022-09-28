f96f1be3bfec2e4f980461514c8b8199d5af490c
Benjamin Lindley <benjameslindley@gmail.com>
2022-09-28 03:25:37 -0500

    nearest pixel filtering instead of linear. perhaps make it a command line option later?

--- src/main.rs
+++ src/main.rs
@@ -281,8 +281,9 @@ fn create_texture() -> u32 {
         
         gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as _);
         gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as _);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as _);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as _);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as _);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as _);
+
         texture
     }
 }
